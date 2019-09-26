1ae717770d9fbd717dc7978b481ff55a95955f31
varunbansal644 <varunbansal644@gmail.com>
2019-09-25 23:40:16 -0700

    Accessing te values

--- array-filtering.js
+++ array-filtering.js
@@ -1,6 +1,6 @@
 var numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-var x=numbers.filter
- function filter (numbers) {
-        return number % 2 === 0
+var filtered=numbers.filter(evenNumbers)
+ function evenNumbers (numbers) {
+        return numbers % 2 === 0
       }
-      console.log(x)
+ console.log(filtered)
